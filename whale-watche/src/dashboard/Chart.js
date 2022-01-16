b459100d9814eb0ed3a201cdfb15815b3860730d
k-laz <klazarev2000@gmail.com>
2022-01-16 10:15:42 -0800

    last commit before submission

--- whale-watche/src/dashboard/Chart.js
+++ whale-watche/src/dashboard/Chart.js
@@ -27,52 +27,100 @@ const data = [
   createData("24:00", undefined),
 ];
 
-export default function Chart() {
+export default function Chart(props) {
   const theme = useTheme();
 
-  return (
-    <React.Fragment>
-      <Title>Today</Title>
-      <ResponsiveContainer>
-        <LineChart
-          data={data}
-          margin={{
-            top: 16,
-            right: 16,
-            bottom: 0,
-            left: 24,
-          }}
-        >
-          <XAxis
-            dataKey="time"
-            stroke={theme.palette.text.secondary}
-            style={theme.typography.body2}
-          />
-          <YAxis
-            stroke={theme.palette.text.secondary}
-            style={theme.typography.body2}
+  if (props.data) {
+    return (
+      <React.Fragment>
+        <Title>Today</Title>
+        <ResponsiveContainer>
+          <LineChart
+            data={data}
+            margin={{
+              top: 16,
+              right: 16,
+              bottom: 0,
+              left: 24,
+            }}
           >
-            <Label
-              angle={270}
-              position="left"
-              style={{
-                textAnchor: "middle",
-                fill: theme.palette.text.primary,
-                ...theme.typography.body1,
-              }}
+            <XAxis
+              dataKey="time"
+              stroke={theme.palette.text.secondary}
+              style={theme.typography.body2}
+            />
+            <YAxis
+              stroke={theme.palette.text.secondary}
+              style={theme.typography.body2}
             >
-              Transactions
-            </Label>
-          </YAxis>
-          <Line
-            isAnimationActive={false}
-            type="monotone"
-            dataKey="amount"
-            stroke={theme.palette.primary.main}
-            dot={false}
-          />
-        </LineChart>
-      </ResponsiveContainer>
-    </React.Fragment>
-  );
+              <Label
+                angle={270}
+                position="left"
+                style={{
+                  textAnchor: "middle",
+                  fill: theme.palette.text.primary,
+                  ...theme.typography.body1,
+                }}
+              >
+                Transactions
+              </Label>
+            </YAxis>
+            <Line
+              isAnimationActive={false}
+              type="monotone"
+              dataKey="amount"
+              stroke={theme.palette.primary.main}
+              dot={false}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <Title>Today</Title>
+        <ResponsiveContainer>
+          <LineChart
+            data={[]}
+            margin={{
+              top: 16,
+              right: 16,
+              bottom: 0,
+              left: 24,
+            }}
+          >
+            <XAxis
+              dataKey="time"
+              stroke={theme.palette.text.secondary}
+              style={theme.typography.body2}
+            />
+            <YAxis
+              stroke={theme.palette.text.secondary}
+              style={theme.typography.body2}
+            >
+              <Label
+                angle={270}
+                position="left"
+                style={{
+                  textAnchor: "middle",
+                  fill: theme.palette.text.primary,
+                  ...theme.typography.body1,
+                }}
+              >
+                Transactions
+              </Label>
+            </YAxis>
+            <Line
+              isAnimationActive={false}
+              type="monotone"
+              dataKey="amount"
+              stroke={theme.palette.primary.main}
+              dot={false}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </React.Fragment>
+    );
+  }
 }
