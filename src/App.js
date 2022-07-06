308dce9225f850185132fac911ffddb1546322ec
Pavan <pavan.khiani@gmail.com>
2022-07-06 16:55:01 -0700

    aspect ratio fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="flex flex-col bg-slate-200 justify-center h-screen">
+    <div className="flex flex-col bg-slate-200 justify-center min-h-screen">
       <Dashboard />
     </div>
   );
